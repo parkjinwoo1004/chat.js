3988de50f42a3c0f7671568a5af37d83c6e213cb
pjwoo662 <pjwoo662@gmail.com>
2021-03-03 21:49:17 +0900

    git

---
+++ js/chat.js
@@ -0,0 +1,53 @@
+"use strict"
+const socket = io();
+
+const nickname = document.querySelector("#nickname")
+const chatList = document.querySelector(".chatting-list")
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
+
+chatInput.addEventListener("keypress", (event) => {
+    if (event.keycode === 13) {
+        send()
+    }
+})
+
+function send() {
+    const param = {
+        name: nickname.value,
+        msg: chatInput.value
+    }
+    socket.emit("chatting", param)
+}
+
+
+sendButton.addEventListener("click", send)
+
+socket.on("chatting", (data) => {
+    const { name, msg, time } = data;
+    const item = new LiModel(name, msg, time);
+    item.makeLi()
+    displayContainer.scrollTo(0, displayContainer.scrollHeight)
+})
+
+function LiModel(name, msg, time){
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = () => {
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent": "received")
+        const dom = `<span class="profile">
+            <span class="user">${this.name}</span>
+            <img class="image" src="file:///C:/Users/pjwoo/Desktop/%EB%A0%88%EC%9D%B4%EC%82%AC%EC%A7%84/20200813_142748.jpg" alt="any">
+        </span>
+        <span class="message">${this.msg}</span>
+        <span class="time">${this.time}</span>`;
+        li.innerHTML = dom;
+        chatList.appendChild(li)
+    }
+}
+
+
